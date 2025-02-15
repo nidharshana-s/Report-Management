789061a007d19e6dae43f5c43a1ea189cb3dbe89
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2025-02-15 11:20:27 +0530

    registeration has been completed

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/uploadMiddleware');
 const { uploadFile, getFile } = require('../controllers/fileController');
+const { register } = require('../controllers/userController');
 
 // Upload PDF or Image and create a report
 router.post('/upload', upload.single('file'), uploadFile);
 
 // Retrieve PDF or Image by ID
 router.get('/file/:fileId', getFile);
+
+//registeration
+router.post('/register',register);
 
 module.exports = router;
